3e0661a016f9122b03b0be9e46d9ab10ff4b436e
Sundy Diez <origeconas@gmail.com>
2023-12-14 22:17:49 +0800

    chore: updates

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
             const inputs = document.querySelectorAll("input[type='text']");
             inputs.forEach(function(input) {
                 input.classList.remove('active');
-                
             });
 
             if(this.id === field.id) {
                 field.classList.add('active'); 
                 activeInput = this; 
 
+                
                 if(this.id === 'octalField') {
                     disableKeys = ['A', 'B', 'C', 'D', 'E', 'F', '8', '9']; 
                 }else if(this.id === 'binaryField') {
@@ -75,7 +75,8 @@ function handleInput(inputField) {
 
 function appendToField(targetFieldId, value) {
     var accc = document.getElementById(activeInput.id); 
-
+    console.log(accc)
+    accc.blur(); 
     if (accc.tagName === 'INPUT' && accc.type === 'text') {
 
         if (value === 'AC') {
@@ -84,6 +85,7 @@ function appendToField(targetFieldId, value) {
             accc.value = accc.value.slice(0, -1);
         } else {
             appendHexToField(accc, value);
+            // console.log('activeInput', activeInput)
         }
         handleInput(accc);
     }
@@ -103,14 +105,17 @@ function appendToField(targetFieldId, value) {
     // }
 }
 
+
 function appendHexToField(field, value) {
+
     var currentText = field.value;
+    console.log(field.value)
     var cursorPosition = field.selectionStart;
     var newText =
         currentText.substring(0, cursorPosition) +
         value +
         currentText.substring(cursorPosition);
-    field.value = newText;
+    field.value = newText; 
 }
 
 function convertFromBinary(value, targetFieldId) {
